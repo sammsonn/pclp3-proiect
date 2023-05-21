f09e2d9a76e8c00b15b0de0c102c5f40d4237a62
Sam <defvlt1337@protonmail.com>
2023-05-21 16:13:15 +0300

    changed input structure, used queues

--- main.c
+++ main.c
@@ -1,18 +1,4 @@
-// TODO - eliberare memorie, coada, headere, makefile
-
-/* exemplu de input
-
-his inv w di in* an interv , it was rather interes .
-add_word arrow
-add_word discussed
-add_word hi
-del_word hi
-show_dict
-priority_word arrow 6
-show_dict
-exit
-
-*/
+// TODO: eliberare memorie
 
 #include <stdio.h>
 #include <string.h>
@@ -24,90 +10,104 @@ exit
 
 int main()
 {
-    int n, m, i, corespunde, steluta;
-    char cuvant[MAX_WORDS][MAX_LENGTH], cuvbun[MAX_LENGTH];
+    int n = 0, corespunde, steluta;
+    char cuvant[MAX_LENGTH], cuvbun[MAX_LENGTH], text[MAX_WORDS], *aux, line[MAX_LENGTH + 3];
+    char *priority;
     struct dictionary_entry *intrare;
+    FILE *fr, *fw;
+    tQueue queue;
 
-    // citește numărul de cuvinte din dicționarul inițial
-    scanf("%d", &n);
-    getchar();
+    fr = fopen("input.in", "r");
+    fw = fopen("output.out", "w");
 
-    intrare = malloc(n * sizeof(struct dictionary_entry));
+    queue = initQueue(queue);
 
-    // citește cuvintele din dicționarul inițial
-    for (i = 0; i < n; i++)
+    fgets(text, MAX_WORDS, fr);
+    text[strlen(text) - 1] = '\0';
+
+    aux = strtok(text, " ");
+    while (aux != NULL)
     {
-        citeste(&intrare[i].word);
-        intrare[i].priority = 0;
+        queue = addQueue(queue, aux);
+        //puts(aux);
+        aux = strtok(NULL, " ");
     }
 
-    // citește numărul de cuvinte introduse de utilizator
-    scanf("%d", &m);
-    getchar();
+    intrare = malloc(sizeof(struct dictionary_entry));
 
-    // citește cuvintele introduse de utilizator
-    for (i = 0; i < m; i++)
+    fgets(line, MAX_LENGTH+3, fr);
+    line[strlen(line) - 1] = '\0';
+    while (strcmp(line, "complete"))
     {
-        scanf("%s", cuvant[i]);
-        getchar();
+        aux = strtok(line, " ");
+        priority = strtok(NULL, " ");
+        adaugacuvant(&intrare, &n, aux, atoi(priority));
+
+        fgets(line, MAX_LENGTH+3, fr);
+        line[strlen(line) - 1] = '\0';
     }
 
     // parcurge cuvintele introduse de utilizator
-    for (i = 0; i < m; i++)
+    while (!isEmptyQueue(queue))
     {
+        queue = delQueue(queue, cuvant);
+
         // verifică dacă are * la final
         steluta = 0;
-        if (cuvant[i][strlen(cuvant[i]) - 1] == '*')
+        if (cuvant[strlen(cuvant) - 1] == '*')
         {
             steluta = 1;
-            cuvant[i][strlen(cuvant[i]) - 1] = '\0';
+            cuvant[strlen(cuvant) - 1] = '\0';
         }
 
         // dacă nu este semn de punctuație
-        if (trbcautat(cuvant[i]))
+        if (trbcautat(cuvant))
         {
             // verifică dacă corespunde măcar unui cuvânt din dicționar
-            corespunde = cautare(intrare, cuvant[i], n);
+            corespunde = cautare(intrare, cuvant, n);
 
             // dacă nu corespunde niciunuia și nu are * la final
             if (!corespunde && !steluta)
             {
-                adaugacuvant(&intrare, &n, cuvant[i]);
+                adaugacuvant(&intrare, &n, cuvant, 1);
 
                 // este afișat așa cum e
-                printf("%s ", cuvant[i]);
+                fprintf(fw, "%s ", cuvant);
             }
 
             // dacă are * la final
             if (steluta)
             {
                 // dacă nu există deja în dicționar
-                if (!exista(&intrare, cuvant[i], &n))
+                if (!exista(&intrare, cuvant, &n))
                 {
-                    adaugacuvant(&intrare, &n, cuvant[i]);
+                    adaugacuvant(&intrare, &n, cuvant, 1);
                 }
 
                 // este afișat așa cum e
-                printf("%s ", cuvant[i]);
+                fprintf(fw, "%s ", cuvant);
             }
 
             // dacă corespunde măcar unui cuvânt
             // din dicționar și nu are * la final
             if (corespunde && !steluta)
             {
-                potrivire(&intrare, cuvant[i], n, cuvbun);
+                potrivire(&intrare, cuvant, n, cuvbun);
 
                 // este afișat cuvântul din dicționar cel mai potrivit
-                printf("%s ", cuvbun);
+                fprintf(fw, "%s ", cuvbun);
             }
         }
         // dacă e semn de punctuație
         else
         {
             // este afișat așa cum e
-            printf("%s ", cuvant[i]);
+            fprintf(fw, "%s ", cuvant);
         }
     }
+
+    fclose(fr);
+    fclose(fw);
 
     return 0;
 }
--- structs.h
+++ structs.h
@@ -1,3 +1,5 @@
+#define MAX_LENGTH 22
+
 struct dictionary_entry
 {
     char *word;
@@ -6,7 +8,7 @@ struct dictionary_entry
 
 typedef struct queueCell // celula din coada
 {
-    char op[20];            // operatia continuta
+    char word[MAX_LENGTH];  // cuvantul continut
     struct queueCell *next; // adresa celulei urmatoare
 } queueCell, *pQueueCell;
 
@@ -15,10 +17,15 @@ typedef struct queue // coada
     pQueueCell front, rear; // adresa fetei si a spatelui
 } tQueue;
 
-void citeste(char **cuvant);
 int cautare(struct dictionary_entry *intrare, char *decautat, int n);
 int trbcautat(char *cuvant);
-void adaugacuvant(struct dictionary_entry **intrare, int *marime, char *cuvant);
 int exista(struct dictionary_entry **intrare, char *decautat, int *marime);
+int isEmptyQueue(tQueue q);
 void potrivire(struct dictionary_entry **intrare,
                char *decautat, int n, char *cuvbun);
+void citeste(char **cuvant);
+void adaugacuvant(struct dictionary_entry **intrare, int *marime, char *cuvant, int priority);
+void freeQueue(tQueue q);
+tQueue initQueue();
+tQueue addQueue(tQueue q, char *op);
+tQueue delQueue(tQueue q, char op[MAX_LENGTH]);
